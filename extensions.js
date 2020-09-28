4f6d06079edc2d5638ed894fbc5268f5db344295
coolemur <coolemur@gmail.com>
2020-09-28 07:12:33 +0300

    Unused extension removed

--- extensions.js
+++ extensions.js
@@ -1,17 +1,5 @@
 Object.assign(String.prototype, {
-  repeatSpaced(times) {
-    return `${this} `.repeat(times).trim();
-  }
-});
-
-Object.assign(String.prototype, {
   toFirstLetterUpperCase() {
     return `${this}`.charAt(0).toUpperCase() + `${this}`.slice(1)
-  }
-});
-
-Object.assign(Array.prototype, {
-  distinct() {
-    return [...new Set(this)];
   }
 });
